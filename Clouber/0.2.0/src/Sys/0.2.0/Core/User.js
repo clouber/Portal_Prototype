e7d4a774d6e108f02310fe4a05c20a5106644f3c
Jon <Jon@352f1864-06c9-a440-a7d0-b03ea0214f00>
2012-06-23 10:03:52 +0000

    git-svn-id: svn://localhost@52 352f1864-06c9-a440-a7d0-b03ea0214f00

---
+++ Clouber/0.2.0/src/Sys/0.2.0/Core/User.js
@@ -0,0 +1,105 @@
+/**
+* @fileOverview Clouber user class.
+* @copyright (c) 20012 by Clouber.org. All rights reserved.
+* @author Jon Zhou
+* @module Cache
+* @requires Clouber.* Clouber.Sys.Core.*
+*/
+
+
+/**
+* Clouber system core modules, using namespace Clouber.Sys.Core
+* @class  Clouber.Sys.Core
+* @module Clouber.Sys.Core
+* @namespace Clouber.Sys.Core
+*/
+Clouber.namespace("Clouber.Sys.Core");
+
+/**
+* Clouber user class.
+* @class  Clouber.Sys.Core.User
+* @namespace Clouber
+* @extends Clouber.BaseObject
+* @constructor
+*/
+Clouber.Sys.Core.User = function () {
+    "use strict";
+
+    /**
+    * The private key of this user.
+    * @property {string} _key
+    * @private
+    * @ignore
+    */
+    var _key = "CLOUBER_PRIVATE",
+
+    /**
+    * The user id.
+    * @property {string} _user
+    * @private
+    * @ignore
+    */
+        _user = "public",
+
+     /**
+    * The user display name.
+    * @property {string} _name
+    * @private
+    * @ignore
+    */
+        _name = "public";
+
+    /**
+    * Get private key
+    * @function key
+    * @param {string} text Uncrypted text.
+    * @return {string} encrypted text
+    */
+    this.key = function (key) {
+        if (!Clouber.isEmpty(key)) {
+            _key = key;
+        }
+        return _key;
+    };
+
+    /**
+    * Get user id
+    * @function id
+    * @param {string} text Uncrypted text.
+    * @return {string} encrypted text
+    */
+    this.id = function (id) {
+        if (!Clouber.isEmpty(id)) {
+            _user = id;
+        }
+        return _user;
+    };
+    
+    /**
+    * Get user name
+    * @function name
+    * @param {string} text Uncrypted text.
+    * @return {string} encrypted text
+    */
+    this.name = function (name) {
+        if (!Clouber.isEmpty(name)) {
+            _name = name;
+        }
+        return _name;
+    };
+};
+Clouber.extend(Clouber.Sys.Core.User, Clouber.BaseObject);
+
+Object.defineProperty(Clouber, "user", {
+    configurable: false,
+    enumerable: true,
+    get: function () {
+        this._user = (this.isNull(this._user)) ? new Clouber.Sys.Core.User() :
+            this._user;
+        return this._user;
+    },
+    set: function (value) {
+        return;
+    }
+});
+
